13609dd8581bc47c73c479635cccdd33521dcd11
Ken Messmer <kenmessmerjr@gmail.com>
2019-05-28 13:31:16 -0500

    Not much done at all.  Just added the notes and made several failed attempts at TODO #1

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -16,12 +17,27 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
-    @RequestMapping(value = "")
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "search/results", method = RequestMethod.POST)
+    public String processSearch(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        model.addAttribute(searchType, searchTerm);
+        model.addAttribute(JobData.findAll());
+        model.addAttribute("columns", ListController.columnChoices);
 
+
+        return "search";
+    }
+    // XXXAdd another results handler method to SearchController, overloading the existing method.
+    // XXXThe method should take in two parameters, specifying the type of search and the search term
+    // In order for the parameters to be properly passed in by Spring Boot, you'll need to name them appropriately, based on the corresponding form field names.
+    // You'll also need to use the correct annotations for the method and parameters.
+    // To configure the correct mapping route, refer to the form action in search.html.
+    // After looking up the search results via the JobData class, you'll need to pass them into the search.html view via the model
+    // You'll also need to pass ListController.columnChoices to the view, as the existing search handler does.
 }
