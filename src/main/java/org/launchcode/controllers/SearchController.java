7fd83dfa607d3fafb3fc8303f3e3b5407d187743
Ken Messmer <kenmessmerjr@gmail.com>
2019-05-28 21:21:57 -0500

    Not much done at all.  Just added the notes and made several failed attempts at TODO #1

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.constraints.Null;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -22,20 +24,20 @@ public class SearchController {
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
     }
-
     // TODO #1 - Create handler to process search request and display results
-    @RequestMapping(value = "search/results", method = RequestMethod.POST)
-    public String processSearch(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
-        model.addAttribute(searchType, searchTerm);
-        model.addAttribute(JobData.findAll());
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         model.addAttribute("columns", ListController.columnChoices);
 
+        ArrayList listylist = JobData.findByColumnAndValue(searchType, searchTerm);
+        model.addAllAttributes(listylist);
 
         return "search";
     }
+
     // XXXAdd another results handler method to SearchController, overloading the existing method.
     // XXXThe method should take in two parameters, specifying the type of search and the search term
-    // In order for the parameters to be properly passed in by Spring Boot, you'll need to name them appropriately, based on the corresponding form field names.
+    // XXXIn order for the parameters to be properly passed in by Spring Boot, you'll need to name them appropriately, based on the corresponding form field names.
     // You'll also need to use the correct annotations for the method and parameters.
     // To configure the correct mapping route, refer to the form action in search.html.
     // After looking up the search results via the JobData class, you'll need to pass them into the search.html view via the model
